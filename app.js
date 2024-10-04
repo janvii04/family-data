c53957feb56f12ac2e8b55ddab1790d8e636b706
janvii04 <jeevan13798@gmail.com>
2024-10-04 14:32:39 +0530

    task1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 1234
+const PORT = 3000
 const multer = require('multer')
 const path = require('path')
 
--- models/userFamily.js
+++ models/userFamily.js
@@ -1,14 +1,19 @@
-module.exports = (Sequelize, sequelize, DataTypes) => {
+
+  module.exports = (Sequelize, sequelize, DataTypes) => {
     return sequelize.define(
       "userFamily",
       {
         ...require("./cors")(Sequelize, DataTypes),
-        userFamilyName: DataTypes.STRING(255),
-        allowNull: false,
+  
+        userFamilyName: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+        },
       },
       {
         tableName: "userFamily",
       }
     );
   };
+  
   
